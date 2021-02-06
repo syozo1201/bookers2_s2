c4bffd20e8b000ed910a78fb3dfdf1e3765664f3
syozo1201 <00syozo1201@gmail.com>
2021-02-06 05:58:41 +0000

    フォロー機能

---
+++ bookers2_s2/config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root :to => "homes#top"
+  get "home/about" => "homes#about"
+  
+  post 'follow/:id' => 'relationships#follow', as: 'follow' 
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+  
+  resources :users, only: [:index, :show, :edit, :update] do
+    member do
+     get :follows, :followers
+    end
+  end
+    
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  
+end
