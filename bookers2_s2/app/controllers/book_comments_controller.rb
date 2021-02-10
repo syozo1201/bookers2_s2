1e267114a04e38ec65b9bad44c749acc1e5cc300
syozo1201 <00syozo1201@gmail.com>
2021-02-10 10:03:01 +0000

    非同期いいねコメント

--- bookers2_s2/app/controllers/book_comments_controller.rb
+++ bookers2_s2/app/controllers/book_comments_controller.rb
@@ -7,7 +7,6 @@ class BookCommentsController < ApplicationController
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
 		if @book_comment.save
-  		redirect_to book_path(@book.id)
 		else
 		  render 'books/show'
 		end
@@ -17,7 +16,6 @@ class BookCommentsController < ApplicationController
 		@book = Book.find(params[:book_id])
   	    book_comment = @book.book_comments.find(params[:id])
 		book_comment.destroy
-		redirect_to request.referer
 	end
 
 	private
